6fb0b951f76f091b75e09045caa94783a29d4253
ashfall <ashwini.oruganti@gmail.com>
2014-07-29 20:30:53 -0700

    Typo, I'm sorry

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     version="0.0",
     description="tls",
     long_description="tls",
-    setup_requires=[
+    install_requires=[
         "construct",
         "enum34",
         "characteristic",
