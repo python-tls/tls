4ea6aa56538ef049ccc74b48ca66fa3493ea8c87
ashfall <ashwini.oruganti@gmail.com>
2014-08-21 23:06:37 -0700

    Happy now?

--- tls/test/test_hello_messages.py
+++ tls/test/test_hello_messages.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
 from tls.hello_message import (
-    ClientHello,  CompressionMethod, HelloRequest, parse_client_hello,
+    ClientHello, CompressionMethod, HelloRequest, parse_client_hello,
     parse_hello_request
 )
 
