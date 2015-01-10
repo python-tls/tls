84e6706ec587469490e4a5a0f82ea90b9e2edb62
Terry Chia <terrycwk1994@gmail.com>
2015-01-10 21:14:38 +0800

    Add tests.

--- tls/test/test_ciphersuites.py
+++ tls/test/test_ciphersuites.py
@@ -2,10 +2,32 @@
 # 2.0, and the BSD License. See the LICENSE file in the root of this repository
 # for complete details.
 
-from tls.ciphersuites import CipherSuites
+import pytest
+
+from tls.ciphersuites import CipherSuites, select_preferred_ciphersuite
+from tls.exceptions import UnsupportedCipherException
 
 
 def test_ciphersuites():
     # TODO: This is to fulfill test coverage. Either make more useful later or
     # remove once the CipherSuites enum gets used in other places.
     assert CipherSuites.TLS_DHE_RSA_WITH_AES_256_CBC_SHA256.value == 0x006B
+
+
+def test_select_preferred_ciphersuite():
+    assert select_preferred_ciphersuite([
+        CipherSuites.TLS_DH_anon_EXPORT_WITH_DES40_CBC_SHA,
+        CipherSuites.TLS_DH_anon_EXPORT_WITH_RC4_40_MD5
+    ], [
+        CipherSuites.TLS_DH_anon_EXPORT_WITH_RC4_40_MD5,
+        CipherSuites.TLS_DH_anon_EXPORT_WITH_DES40_CBC_SHA
+    ]) == CipherSuites.TLS_DH_anon_EXPORT_WITH_RC4_40_MD5
+
+
+def test_unsupported_ciphersuite():
+    with pytest.raises(UnsupportedCipherException):
+        select_preferred_ciphersuite([
+            CipherSuites.TLS_DH_anon_EXPORT_WITH_DES40_CBC_SHA
+        ], [
+            CipherSuites.TLS_DH_anon_EXPORT_WITH_RC4_40_MD5
+        ])
