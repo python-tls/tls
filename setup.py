166d6591a9e9246f774e8ef6ab0a295f891e8b30
Mark Williams <markrwilliams@gmail.com>
2016-09-26 08:43:39 -0700

    Pin construct dependency to version 2.5.3. (#97)
    Versions of construct after 2.5.5 include breaking changes to the
    library's public API.  Unfortunately, the latest version publically
    available with 2.5.5's API is 2.5.3
    
    While we sort out what to do about this, we should pin construct to
    version 2.5.3.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     long_description="tls",
     install_requires=[
         "attrs",
-        "construct",
+        "construct==2.5.3",
         "cryptography",
         "enum34",
     ],
